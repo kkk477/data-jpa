68a0267df2f82342afa1bd6081f1187c6d1b5a64
HoWoong <dlghdnd@gmail.com>
2023-07-12 18:30:03 +0900

    섹션 4. 쿼리 메소드 기능. 메소드 이름으로 쿼리 생성.

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
 public class Member {
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 
     @Id @GeneratedValue
     @Column(name = "member_id")
